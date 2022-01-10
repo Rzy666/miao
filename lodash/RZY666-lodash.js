f7aa8c36f02a29c9e76d137dba8212e8135f3ca4
renzhongyuan <1535607348@qq.com>
2022-01-10 15:54:16 +0800

    .js

--- lodash/RZY666-lodash.js
+++ lodash/RZY666-lodash.js
@@ -1,6 +1,20 @@
-var Rzy666 = {
+var rzy666 = {
   chunk: function (array, size) {
-
+    var len = array.length
+    if (len <= size) return array
+    var result = []//设置两个空数组
+        temp = []
+    for (var i = 0; i < len; i++){
+      temp.push(array[i])//前面的数字存到第一个数组里
+      if (temp.length == size) {//如果到了size个，就进入要输出的数组，之后在建立一个新数组
+        result.push(temp)
+        temp = []
+      }
+    }
+     if (temp.length != 0) {//如果最后有剩余，不到size个push进去
+       result.push(temp)
+     }
+    return result
   }
 
 }
