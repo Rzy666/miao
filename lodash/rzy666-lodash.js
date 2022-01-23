ec70fd2370c0ff483bafd0a69959f64688a39194
renzhongyuan <1535607348@qq.com>
2022-01-23 19:22:25 +0800

    ..

--- lodash/rzy666-lodash.js
+++ lodash/rzy666-lodash.js
@@ -1,4 +1,4 @@
-var rzy666 = function() {
+var rzy666 = function () {
   function chunk(array, size) {
     var len = array.length
     if (len <= size) return array
@@ -66,24 +66,24 @@ var rzy666 = function() {
     }
     return result
   }
-  function flatten(ary){
-    return ary.reduce((result , item) => {
+  function flatten(ary) {
+    return ary.reduce((result, item) => {
       if (Array.isArray(item)) {
         result.push(...item)
       } else {
         result.push(item)
       }
       return result
-    },[])
+    }, [])
   }
   function flattenDeep(array) {
     var result = []
-    for (var i = 0; i < array.length; i++){
+    for (var i = 0; i < array.length; i++) {
       if (!Array.isArray(array[i])) {
         result.push(array[i])
       } else {
         var time = flattenDeep(array[i])
-        for (var j = 0; j < time.length; j++){
+        for (var j = 0; j < time.length; j++) {
           result.push(time[j])
         }
       }
@@ -92,13 +92,13 @@ var rzy666 = function() {
   }
   function flattenDepth(array, depth = 1) {
     if (depth == 0) {
-       return  array.slice()
+      return array.slice()
     }
     var res = []
-    for (var i = 0; i < array.length; i++){
+    for (var i = 0; i < array.length; i++) {
       if (Array.isArray(array[i])) {
-        var item = flattenDepth(array[i],depth -1)
-        for (var j = 0; j < item.length; j++){
+        var item = flattenDepth(array[i], depth - 1)
+        for (var j = 0; j < item.length; j++) {
           res.push(item[j])
         }
       } else {
@@ -112,31 +112,31 @@ var rzy666 = function() {
     return array[0]
   }
   function indexOf(array, value, fromIndex = 0) {
-    for (var i = fromIndex; i < array.length; i++){
+    for (var i = fromIndex; i < array.length; i++) {
       if (array[i] == value) {
-          return i
-        }
+        return i
+      }
     }
     return -1
   }
   function initial(array) {
     var res = []
-    for (var i = 0; i < array.length - 1; i++){
+    for (var i = 0; i < array.length - 1; i++) {
       res.push(array[i])
     }
     return res
   }
   function fill(array, value, start = 0, end = array.length) {
-    for (var i = start; i < end; i++){
+    for (var i = start; i < end; i++) {
       array[i] = value
     }
     return array
   }
   function join(array, separator = ',') {
     var str = ''
-    for (var i = 0; i < array.length; i++){
+    for (var i = 0; i < array.length; i++) {
       if (i == array.length - 1) {
-         str = str + array[i]
+        str = str + array[i]
       } else {
         str = str + array[i] + separator
       }
@@ -145,10 +145,10 @@ var rzy666 = function() {
   }
   function last(array) {
     var len = array.length - 1
-    return array[len ]
+    return array[len]
   }
   function lastIndexOf(array, value, fromIndex = array.length - 1) {
-    for (var i = fromIndex; i >= 0; i--){
+    for (var i = fromIndex; i >= 0; i--) {
       if (array[i] == value) {
         return i
       }
@@ -157,7 +157,7 @@ var rzy666 = function() {
   }
   function pull(array, values) {
     var res = []
-    for (var i = 0; i < array.length; i++){
+    for (var i = 0; i < array.length; i++) {
       if (array[i] !== values) {
         res.push(array[i])
       }
@@ -165,15 +165,15 @@ var rzy666 = function() {
     return res
   }
   function reverse(array) {
-    var res  =  []
-    for (var i = 0; i < array.length; i++){
+    var res = []
+    for (var i = 0; i < array.length; i++) {
       res.unshift(array[i])
     }
     return res
   }
   function tail(array) {
-    var res  = []
-    for (var i = 1; i < array.length; i++){
+    var res = []
+    for (var i = 1; i < array.length; i++) {
       res.push(array[i])
     }
     return res
@@ -183,49 +183,49 @@ var rzy666 = function() {
     if (array.length <= n) {
       return array
     }
-    for (var i = 0; i < n; i++){
-    res.push(array[i])
+    for (var i = 0; i < n; i++) {
+      res.push(array[i])
     }
     return res
   }
-  function takeRight(array,n = 1) {
+  function takeRight(array, n = 1) {
     if (n > array.length) {
       return array
     }
     var res = []
-    for (var i = array.length - 1; i >= array.length - n; i--){
+    for (var i = array.length - 1; i >= array.length - n; i--) {
       res.unshift(array[i])
     }
     return res
   }
- function uniq(array) {//数组去重、
+  function uniq(array) {//数组去重、
     var map = {}
     var res = []
-    for (var i = 0; i < array.length; i++){
+    for (var i = 0; i < array.length; i++) {
       if (array[i] in map) {
-       map[array[i]]++
+        map[array[i]]++
       } else {
-       map[array[i]] = 1
-       res.push(array[i])
+        map[array[i]] = 1
+        res.push(array[i])
       }
     }
     return res
   }
-  uniq : (ary) => Array.from( new Set(ary)),
-  function without(array, ...val) {
-    var res = []
-    array.forEach(item => {//遍历出数组的每一项
-      if (!val.includes(item)){ //item的值不在val中
-        res.push(item)
-      }
-    })
-    return res
-  }
+  uniq: (ary) => Array.from(new Set(ary)),
+    function without(array, ...val) {
+      var res = []
+      array.forEach(item => {//遍历出数组的每一项
+        if (!val.includes(item)) { //item的值不在val中
+          res.push(item)
+        }
+      })
+      return res
+    }
   function zip() {
     let res = []
-    for (var i = 0; i < arguments[0].length; i++){
+    for (var i = 0; i < arguments[0].length; i++) {
       res[i] = []
-      for (var j = 0; j < arguments.length; j++){
+      for (var j = 0; j < arguments.length; j++) {
         res[i][j] = arguments[j][i]
       }
     }
@@ -235,7 +235,7 @@ var rzy666 = function() {
     let result = accumulator ?? 0
     for (let key in collection) {
       let valuer = collection[key]
-      result = iteratee(result , valuer , key)
+      result = iteratee(result, valuer, key)
     }
     return result
   }
@@ -247,7 +247,7 @@ var rzy666 = function() {
     return sum
   }
   function isBoolean(values) {
-    let val = typeof(values)
+    let val = typeof (values)
     if (values === null) {
       return false
     }
@@ -261,31 +261,31 @@ var rzy666 = function() {
     for (let key in value) {
       res.push(value[key])
     }
-    return  res
+    return res
   }
   function max(array) {
     var max = array[0]
     if (max == null) {
       return undefined
     }
-    for (var i = 0; i < array.length; i++){
+    for (var i = 0; i < array.length; i++) {
       if (array[i] > max) {
         max = array[i]
       }
     }
-     return max
+    return max
   }
   function sum(array) {
-  return array.reduce((sum , val) => sum += val)
+    return array.reduce((sum, val) => sum += val)
   }
   function isNil(value) {
-    return value == null|| value == undefined
+    return value == null || value == undefined
   }
   function isNull(value) {
     return value == null
   }
   function isNUmber(value) {
-     return  typeof(value) === 'number'
+    return typeof (value) === 'number'
   }
   function toArray(value) {
     var res = []
@@ -299,42 +299,53 @@ var rzy666 = function() {
       return undefined
     }
     var max = -Infinity
-    for (var i = 0; i < array.length; i++){
+    for (var i = 0; i < array.length; i++) {
       if (array[i] > max) {
-              max =array[i]
+        max = array[i]
       }
     }
     return max
   }
-   function repeat(string = '', n = 1){
-    let result =''
+  function repeat(string = '', n = 1) {
+    let result = ''
     for (var i = 0; i < string.length; i++) {
       result += string
     }
     return result
   }
-  function  concat(array, ...ary) {
+  function concat(array, ...ary) {
     for (var i in ary) {
       var p = ary[i]
       if (Array.isArray(p)) {
         for (let j in p {
-        array.push(p[j])
-      }
+          array.push(p[j])
+        }
       } else {
         array.push(p)
-    }
+      }
     }
     return array
+  }
+  function nth(array, n = 0) {
+    if (n >= 0) {
+      return array[n]
+    } else {
+      return array[array.length + n]
+    }
+  }
+  function pullall(array, values) {
+
   }
 
 
   return {
+  
     chunk, compact, difference, drop, dropRight, flatten, flattenDeep, flattenDepth,
-    head, indexOf, initial, fill, join, last, lastIndexOf, pull,
+    head, indexOf, initial, fill, join, last, lastIndexOf, nth, pull,
     reverse, tail, take, takeRight, uniq, without, zip, reduce, size, isBoolean, isEmpty, toArray, max, sum, isNil, isNull, isNumber,
     repeat, concat
-  }
 
+  }
 }()
 
 
